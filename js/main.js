33343d9f1f0c65234bca2576dd6acc6ea3236cb7
Brigitta Glanz <glanzb@icloud.com>
2014-10-14 13:21:26 -0700

    overlay

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var $overlay = $('<div id="overlay"></div>');
+var $form = $("<form>");
+
+//add form to overlay
+$overlay.append($form);
+
+//Add overlay
+$("body").append($overlay);
+
+
+//Capture the click event on a link to an image
+$("#add-new a").click(function(event){
+  event.preventDefault();
+  var formLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $form.attr("src", formLocation);
+  
+  //Show the overlay.
+  $overlay.show();
+  
+
+//When overlay is clicked
+$overlay.click(function(){
+  //Hide the overlay
+  $overlay.hide();
+});
